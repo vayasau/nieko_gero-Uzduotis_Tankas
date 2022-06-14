5a164d677decbf9fd6283aac1f19c354c0ce2cb0
vaysau <pamirstaspastas@gmail.com>
2022-06-14 16:53:32 +0300

    nieko_gero

--- tankas.py
+++ tankas.py
@@ -3,7 +3,7 @@ class Tankas:
         self.kryptis = kryptis
         self.koordinates = koordinates
         self.suviai = suviai
-        self.visi_suviai = {}
+        self.visi_suviai = {"Siaure" : 0, "Rytai" : 0, "Pietus" : 0, "Vakarai" : 0}
         self.kryptys = ["Siaure", "Rytai", "Pietus", "Vakarai"]
         
     def info(self):
@@ -17,17 +17,17 @@ class Tankas:
 
     def pirmyn(self):
         self.koordinates[0] += 1
-        #tankas.info()
+        tankas.info()
        
     def atgal(self):
         self.koordinates[0] -= 1
         self.kryptis = self.kryptys[self.kryptys.index(self.kryptis)-2]
-        #tankas.info()
+        tankas.info()
  
     def kairen(self):
         self.koordinates[1] -= 1
         self.kryptis = self.kryptys[self.kryptys.index(self.kryptis)-1]
-        #tankas.info()
+        tankas.info()
 
     def desinen(self):
         self.koordinates[1] += 1
@@ -35,20 +35,22 @@ class Tankas:
             self.kryptis = self.kryptys[0]
         else:
             self.kryptis = self.kryptys[self.kryptys.index(self.kryptis)+1]
-        #tankas.info()
+        tankas.info()
     
        
     def suvis(self):
         if self.kryptis in self.visi_suviai:
             self.visi_suviai[self.kryptis] = self.visi_suviai[self.kryptis]+1
-        else:
-            self.visi_suviai[self.kryptis] = 1 
+        # else:
+        #     self.visi_suviai[self.kryptis] = 1 
 
         self.suviai = sum(self.visi_suviai.values())
-        print(self.visi_suviai)
+        # print(self.visi_suviai)
+        tankas.info()
 
 
 tankas = Tankas("Siaure", [5,5], 0)
+tankas.info()
 
 while True:
     pasirinkimas = int(input("1 - Pirmyn, \n2 - Atgal, \n3 - Kairen, \n4 - Desinen, \n5 - Suvis, \n6 - Info, \n9 - išeiti iš programos\n"))
